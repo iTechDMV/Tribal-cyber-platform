8b6af697c1e63f2624185ccb53da6f27ba7e21cf
Dakota Kimble-Curry <145517847+iTechDMV@users.noreply.github.com>
2026-05-11 22:59:01 -0400

    Add Table component to render columns and rows

---
+++ Table.jsx
@@ -0,0 +1,25 @@
+export default function Table({ columns, rows }) {
+  return (
+    <div className="table-wrap">
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((c) => (
+              <th key={c}>{c}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {rows.map((row, i) => (
+            <tr key={i}>
+              {row.map((cell, j) => (
+                <td key={j}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
